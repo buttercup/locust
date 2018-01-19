94eec0a14ebd056a61ca9d1eaca4eac89fe57b63
Perry Mitchell <perry@perrymitchell.net>
2018-01-19 20:52:32 +0200

    Remove default export

--- source/index.js
+++ source/index.js
@@ -1,14 +1,6 @@
-import { getLoginTarget, getLoginTargets } from "./loginTargets.js";
 import { getSharedObserver } from "./UnloadObserver.js";
 
 // Initialise the DOM unload observer
 getSharedObserver();
 
-/**
- * The Locust library
- * @module Locust
- */
-export default {
-    getLoginTarget,
-    getLoginTargets
-};
+export { getLoginTarget, getLoginTargets } from "./loginTargets";
