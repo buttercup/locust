0d081c624e49d5caf7e5a9e16f1a8da97ab3cfbd
Perry Mitchell <perry@perrymitchell.net>
2019-01-12 11:35:31 +0200

    Add dev mode for local testing

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,19 @@ const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 const DIST = path.resolve(__dirname, "./dist");
 const SOURCE = path.resolve(__dirname, "./source");
+const DEV_SRC = path.resolve(__dirname, "./dev");
+const INJECT_MODE = process.env.INJECT === "yes";
+
+if (INJECT_MODE) {
+    console.log("Running injection-script build...");
+    console.log("Inject the following:");
+    console.log(
+        `(function() { var s = document.createElement("script"); s.src="http://localhost:8081/locust.js"; document.body.appendChild(s); })()`
+    );
+}
 
 const config = {
-    entry: path.join(SOURCE, "./index.js"),
+    entry: path.join(INJECT_MODE ? DEV_SRC : SOURCE, "./index.js"),
 
     output: {
         filename: "locust.js",
